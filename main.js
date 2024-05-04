a9cbf38ad543f949ee795fc52609671297b1725a
Alex Horing <106245550+AlexHoring07@users.noreply.github.com>
2024-05-04 09:44:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ function diminuiTamanho() {
     }
     numeroSenha.textContent = tamanhoSenha;
 }
-
 function aumentaTamanho() {
     if (tamanhoSenha < 20) {
             // tamanhoSenha = tamanhoSenha + 1;
